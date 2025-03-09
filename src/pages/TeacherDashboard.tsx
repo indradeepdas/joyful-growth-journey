0627cfcca9c07eb5e6775a8bce4645293075e734
indradeepdas <indradeep.das@gmail.com>
2025-03-09 21:36:19 +0100

    Create TeacherDashboard.tsx

---
+++ src/pages/TeacherDashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TeacherDashboard() {
+  return (
+    <div>
+      <h1>Teacher Dashboard</h1>
+      <p>Welcome to the teacher dashboard. This section is under development.</p>
+    </div>
+  );
+}
+
+export default TeacherDashboard;
