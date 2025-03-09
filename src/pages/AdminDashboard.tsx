d27720ed2f2263d35b683be79bd3459ee602c06f
indradeepdas <indradeep.das@gmail.com>
2025-03-09 21:37:10 +0100

    Add AdminDashboard component

---
+++ src/pages/AdminDashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AdminDashboard() {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <p>Welcome to the admin dashboard. This section is under development.</p>
+    </div>
+  );
+}
+
+export default AdminDashboard;
